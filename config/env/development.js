8174077afa690dee835f4d601fa2ec63b8d3a649
TJ <tj@ThananjeyanRs-MacBook-Pro.local>
2017-12-07 22:09:59 +0530

    local file url updated to mlab

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,8 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    //uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://meanuser:meanpass@ds129156.mlab.com:29156/meandb',
     options: {
       user: '',
       pass: ''
